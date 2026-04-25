0145d682a353de105faa324f544068fa18f08feb
aaksgup <aksgup03@gmail.com>
2026-04-25 18:37:36 -0400

    Add search feature test queries

---
+++ sql/test_queries.sql
@@ -0,0 +1,5 @@
+SELECT * FROM employees WHERE empid = 1;
+SELECT * FROM employees WHERE Fname = 'Snoopy';
+SELECT * FROM employees WHERE Lname = 'Brown';
+SELECT * FROM employees WHERE SSN = '123456789';
+SELECT * FROM employees WHERE empid = 99999;
